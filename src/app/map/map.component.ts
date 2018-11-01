7ee4b826760b34ff380df517ab33b8cbe06f4ff0
ivanbodnar <ibodnar@outlook.com>
2018-11-01 07:45:48 -0300

    Added map init to map.component.ts (test)

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const L;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -10,6 +12,11 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
   }
 
 }
