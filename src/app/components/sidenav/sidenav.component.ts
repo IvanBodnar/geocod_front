e40db71388ebd0161f7999b5d10fc0c158c6e827
ivanbodnar <ibodnar@outlook.com>
2018-11-01 23:18:11 -0300

    SidenavComponent injected with StreetsService to receive streets array. setTimeout used on ngOnInit to wait a cicle

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StreetsService } from '../../services/streets.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private streetService: StreetsService
+  ) { }
 
   ngOnInit() {
+    setTimeout(
+      () => {
+        console.log(this.streetService.streetsArray);
+      }
+    );
+
   }
 
 }
