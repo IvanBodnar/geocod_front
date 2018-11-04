a94ef071ca0f26f6e909d2632699580cba641ca1
ivanbodnar <ibodnar@outlook.com>
2018-11-04 17:58:47 -0300

    FormGroup added in form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  formGroup = new FormGroup({
+    calle1: new FormControl(),
+    calle2: new FormControl()
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: any) {
+    console.log(form);
   }
 
 }
