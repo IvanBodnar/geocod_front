ceb2661720ceb42a76a756830b52f30877815626
ivanbodnar <ibodnar@outlook.com>
2018-11-04 21:36:52 -0300

    Refactor: formGroup -> streetsForm - FormComponent.onSubmit calls dataService.getIntersection

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup } from '@angular/forms';
+import {StreetsService} from '../../services/streets.service';
+import {DataService} from '../../services/data.service';
+import IntersectionModel from '../../models/intersection.model';
 
 @Component({
   selector: 'app-form',
@@ -8,18 +11,25 @@ import {FormControl, FormGroup } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
 
-  formGroup = new FormGroup({
+  streetsForm = new FormGroup({
     calle1: new FormControl(),
     calle2: new FormControl()
   });
 
-  constructor() { }
+  constructor(
+    private streetsService: StreetsService,
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
   }
 
-  onSubmit(form: any) {
-    console.log(form);
+  onSubmit(form: FormGroup) {
+    const intersection = new IntersectionModel(form.controls.calle1.value, form.controls.calle2.value);
+    this.dataService.getIntersection(intersection)
+      .subscribe(
+        result => console.log(result)
+      )
   }
 
 }
