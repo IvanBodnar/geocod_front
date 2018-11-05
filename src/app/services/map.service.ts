fae9ad3b4090ff9eb6d8f747c308fc627e33a44b
ivanbodnar <ibodnar@outlook.com>
2018-11-04 23:28:29 -0300

    MapService.getStreetsToIntersect method created -> subscribes to dataService.getIntersection

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import {IntersectionPoint, IntersectionResponse, StreetsToIntersectModel} from '../models/intersection.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  getStreetsToIntersect(streets: StreetsToIntersectModel): void {
+    this.dataService.getIntersection(streets)
+      .subscribe(
+        (intersection: IntersectionResponse) => console.log(new IntersectionPoint(intersection).getLatLong()),
+        error => console.log(error)
+      );
+  }
 }
