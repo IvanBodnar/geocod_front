f7de42e359926aa58b6dc53fb271a70eb4ba8e90
ivanbodnar <ibodnar@outlook.com>
2018-11-04 23:26:08 -0300

    Rename class IntersectionModel -> StreetsToIntersectModel - Added interfaces LatLong and IntersectionResponse - Added class IntersectionPoint

--- src/app/models/intersection.model.ts
+++ src/app/models/intersection.model.ts
@@ -1,7 +1,35 @@
 
-export default class IntersectionModel {
+interface LatLong {
+  lat: string;
+  lng: string;
+}
+
+export interface IntersectionResponse {
+  intersection: string;
+}
+
+export class StreetsToIntersectModel {
   constructor(
     public street1: string,
     public street2: string
   ) { }
+
+}
+
+export class IntersectionPoint {
+  constructor(
+    public response: IntersectionResponse
+  ) { }
+
+  getLatLong(): LatLong {
+    let lat: string;
+    let lng: string;
+    [lng, lat] = this.response
+      .intersection
+      .replace('POINT(', '')
+      .replace(')', '')
+      .split(' ');
+
+    return <LatLong>{ lat, lng };
+  }
 }
